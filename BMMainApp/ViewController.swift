08a1c71a753b43d2186c20c001a405d3454781f0
Javier Bolaños Carapia <javier.bc121086@gmail.com>
2022-08-14 01:27:29 -0500

    Initial Commit

---
+++ BMMainApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMMainApp
+//
+//  Created by Javier Carapia on 14/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
